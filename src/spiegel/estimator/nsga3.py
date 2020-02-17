3048621c4551e73c750a0dea52046023041734f9
jorshi <jordieshier@gmail.com>
2020-02-17 00:56:23 -0800

    NSGA3 -- needs to be cleaned up something fierce, but it's working!

---
+++ src/spiegel/estimator/nsga3.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+"""
+Non-dominated Sorting Genetic Algorithm with multiple objectives
+"""
+
+import numpy as np
+import random
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+
+from spiegel.evaluation.audio_eval_base import AudioEvalBase
+from spiegel.estimator.estimator_base import EstimatorBase
+from spiegel.synth.synth_base import SynthBase
+from spiegel.features.features_base import FeaturesBase
+
+
+class NSGA3(EstimatorBase):
+    """
+    """
+
+    def __init__(self, synth, featuresList, seed=None):
+        """
+        Constructor
+        """
+        super().__init__()
+
+        if not isinstance(synth, SynthBase):
+            raise TypeError("synth must be of type SynthBase")
+
+        self.synth = synth
+        self.numParams = len(synth.getPatch())
+
+        if not isinstance(featuresList, list):
+            raise TypeError("featuresList must be a list")
+
+        self.featuresList = featuresList
+        self.target = None
+
+        random.seed(seed)
+        self.setup()
+
+
+    def setup(self):
+        """
+        Setup genetic algorithm
+        """
+
+        self.numObjectives = len(self.featuresList)
+
+        # Create individual types
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,) * self.numObjectives)
+        creator.create("Individual", list, fitness=creator.FitnessMin)
+
+        # Setup toolbox
+        self.toolbox = base.Toolbox()
+
+        # Attribute generator
+        self.toolbox.register("attr_float", random.random)
+
+        # Structure initializers
+        self.toolbox.register(
+            "individual",
+            tools.initRepeat,
+            creator.Individual,
+            self.toolbox.attr_float,
+            self.numParams
+        )
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+
+        ref_points = tools.uniform_reference_points(self.numObjectives, 12)
+
+        self.toolbox.register("evaluate", self.fitness)
+        self.toolbox.register("mate", tools.cxTwoPoint)
+        self.toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+        self.toolbox.register("select", tools.selNSGA3, ref_points=ref_points)
+
+
+    def fitness(self, individual):
+        """
+        Evaluate fitness
+        """
+
+        self.synth.setPatch(individual)
+        self.synth.renderPatch()
+        out = self.synth.getAudio()
+
+        errors = []
+        index = 0
+        for extractor in self.featuresList:
+            outFeatures = extractor.getFeatures(out)
+            errors.append(AudioEvalBase.absoluteMeanError(self.target[index], outFeatures))
+            index += 1
+
+        return errors
+
+
+    def predict(self, input):
+        """
+        Run GA prection on input
+        """
+
+        self.target = []
+        for extractor in self.featuresList:
+            self.target.append(extractor.getFeatures(input))
+
+        pop = self.toolbox.population(n=300)
+        invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+        fitnesses = self.toolbox.map(self.toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", np.mean, axis=0)
+        stats.register("std", np.std, axis=0)
+        stats.register("min", np.min, axis=0)
+        stats.register("max", np.max, axis=0)
+
+        logbook = tools.Logbook()
+        logbook.header = "gen", "evals", "std", "min", "avg", "max"
+
+        record = stats.compile(pop)
+        logbook.record(gen=0, evals=len(invalid_ind), **record)
+        print(logbook.stream)
+
+                # Begin the generational process
+        for gen in range(1, 25):
+            offspring = algorithms.varAnd(pop, self.toolbox, 0.5, 0.5)
+
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = self.toolbox.map(self.toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
+
+            # Select the next generation population from parents and offspring
+            pop = self.toolbox.select(pop + offspring, 300)
+
+            # Compile statistics about the new population
+            record = stats.compile(pop)
+            logbook.record(gen=gen, evals=len(invalid_ind), **record)
+            print(logbook.stream)
+
+        return tools.selBest(pop, 1)[0]
