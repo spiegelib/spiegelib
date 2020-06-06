25b09037ab524eb736a0550945120e6f960f15cf
jorshi <jordieshier@gmail.com>
2020-06-05 22:25:13 -0700

    Can load a sound match object, and run sound matching over server

--- src/spiegelib/core/sound_match_server.py
+++ src/spiegelib/core/sound_match_server.py
@@ -2,8 +2,13 @@
 """
 """
 
-
+import os
+import json
+from copy import copy
 from wsgiref.simple_server import make_server
+from urllib.parse import parse_qs
+
+import spiegelib as spgl
 
 
 # pylint: disable=too-many-instance-attributes
@@ -12,14 +17,14 @@ class SoundMatchServer():
     """
 
     # pylint: disable=too-many-arguments
-    def __init__(self, sound_match):
+    def __init__(self, sound_matcher):
         """
         Constructor
         """
 
         self.port = 8000
         self.address = "localhost"
-        self.sound_match = sound_match
+        self.sound_matcher = sound_matcher
 
 
     def start(self):
@@ -55,9 +60,17 @@ class SoundMatchServer():
         """
 
         path = environ['PATH_INFO']
-        print(path)
-        response_body = b''
-        start_response('404 Not Found', SoundMatchServer.get_headers(response_body, 'text/html'))
+
+        if path == "/sound_match":
+            query = parse_qs(environ['QUERY_STRING'])
+            response_body = self.try_sound_match(query, start_response)
+
+        else:
+            response_body = b''
+            start_response(
+                '404 Not Found',
+                SoundMatchServer.get_headers(response_body, 'text/html')
+            )
 
         return [response_body]
 
@@ -69,7 +82,6 @@ class SoundMatchServer():
 
         # the environment variable CONTENT_LENGTH may be empty or missing
         path = environ['PATH_INFO']
-        print(path)
         response_body = b''
         start_response('404 Not Found', SoundMatchServer.get_headers(response_body, 'text/html'))
 
@@ -88,3 +100,53 @@ class SoundMatchServer():
         ]
 
         return response_headers
+
+
+    def try_sound_match(self, query, start_response):
+        """
+        Trys to run a sound match given a query string
+
+        Args:
+            query (dict): Query string, which should contain a link to a target
+                audio file
+        """
+
+        target = query.get('target', None)
+        response_body = b''
+
+        if target is None:
+            start_response(
+                '404 Not Found',
+                SoundMatchServer.get_headers(response_body, 'text/html')
+            )
+            return response_body
+
+        target = os.path.abspath(target[0])
+        audio = spgl.AudioBuffer()
+
+        try:
+            audio.load(target)
+        except FileNotFoundError:
+            start_response(
+                '404 Not Found',
+                SoundMatchServer.get_headers(response_body, 'text/html')
+            )
+            return response_body
+
+        params = []
+        try:
+            _ = self.sound_matcher.match(audio)
+            params = copy(self.sound_matcher.get_patch(skip_overridden=False))
+            self.sound_matcher.patch = None
+        except Exception as e:
+            start_response(
+                '500 Internal Server Error',
+                SoundMatchServer.get_headers(response_body, 'text/html')
+            )
+            return response_body
+
+        params = {'patch': params}
+        params = json.dumps(params)
+        response_body = bytes(params, 'utf-8')
+        start_response('200 Okay', SoundMatchServer.get_headers(response_body, 'application/json'))
+        return response_body
