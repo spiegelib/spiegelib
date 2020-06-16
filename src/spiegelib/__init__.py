81a34809e4eba377f02a666d735b76e1a7de0b96
jorshi <jordieshier@gmail.com>
2020-06-16 12:11:54 -0700

    Adding spiegelib socket server and OSC message handling

--- src/spiegelib/__init__.py
+++ src/spiegelib/__init__.py
@@ -5,6 +5,7 @@ from spiegelib.core.audio_buffer import AudioBuffer
 from spiegelib.core.dataset_generator import DatasetGenerator
 from spiegelib.core.sound_match import SoundMatch
 from spiegelib.core.sound_match_server import SoundMatchServer
+from spiegelib.core.sound_match_socket import SoundMatchSocket
 
 import spiegelib.evaluation
 import spiegelib.estimator
