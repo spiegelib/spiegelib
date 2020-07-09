0194cfd9e71cda47d10d189764feb27d89a0126a
jorshi <jordieshier@gmail.com>
2020-07-09 11:08:07 -0700

    Updating docs to v0.0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, University of Victoria'
 author = 'University of Victoria'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
